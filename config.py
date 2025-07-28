690444ffff1d469b60a5618905a6ca5d76bfbf9d
ttf-1234 <ttfreyer@proton.me>
2025-07-28 11:23:48 +0200

    added functionality to query for several reference rasters

--- config.py
+++ config.py
@@ -1,23 +1,36 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-##### Path of the reference raster #####
-reference_raster_path = os.path.join(BASE_DIR, "data/reference_raster/dem_rofental_100.asc")
-reference_raster_crs = "EPSG:32632"
+##### Directory containing reference rasters #####
+reference_raster_dir = os.path.join(BASE_DIR, "data/reference_raster")
 tile_txt_path = os.path.join(BASE_DIR, "data/tile_system/relevant_tiles.txt")
 
+
+# List your reference rasters here: just the filename and the CRS string.
+# The path will be constructed automatically from reference_raster_dir.
+# Example: {"name": "dem_rofental_100.asc", "crs": "EPSG:32632"}
+reference_rasters = [
+    {"name": "dem_rofental_100.asc", "crs": "EPSG:32632"},
+    {"name": "dem_guadalfeo_100.asc", "crs": "EPSG:32630"},
+    # Add more as needed
+]
+
+# Helper to get full path for a reference raster entry
+def get_reference_raster_path(raster_entry):
+    return os.path.join(reference_raster_dir, raster_entry["name"])
+
 ##### CLMS credentials #####
-clms_username = "xxx"
-clms_password = "xxx"
+clms_username = "xx"
+clms_password = "xx"
 
 ##### CLMS product ID #####
 clms_query_type = "query_and_download"  # Options: "query", "download", "query_and_download"
 # ALLOWED_PRODUCTS = ["FSC", "PSA", "SWS", "WDS", "GFSC"]
-clms_product=["FSC","PSA"]
+clms_product=["FSC"]
 
 ##### Period #####
-start_date = "2023-01-01T00:00:00Z"
-end_date = "2023-01-15T23:59:59Z"
+start_date = "2023-07-01T00:00:00Z"
+end_date = "2023-07-15T23:59:59Z"
 
 ##### Output path for downloaded data #####
 output_path_original = os.path.join(BASE_DIR, "data/clms_data/original/")
@@ -25,7 +38,7 @@ output_path_processed = os.path.join(BASE_DIR, "data/clms_data/processed/")
 
 ##### Further processing #####
 mosaic_output = True
-reclassify = False
+reclassify = True
 crop_resample = True
-filter_cc = True
+filter_cc = False
 cc_threshold = 0.2
